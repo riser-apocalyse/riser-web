995da50c287525d1839a7960e2d3dad3fe530693
abunuwas <joseharoperalta@gmail.com>
2018-04-03 05:39:01 +0100

    Add physical user view to list profiles

---
+++ src/views/main/PhysicalUserListProfiles.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Your registered Profiles</h1>
+    <ListLogicalUsers />
+  </div>
+</template>
+
+<script>
+
+import { ListLogicalUsers } from '../../components/logicalUsers'
+
+export default {
+  name: 'physical-user-profiles',
+  components: {
+    ListLogicalUsers
+    }
+}
+</script>
