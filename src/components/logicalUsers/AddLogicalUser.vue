d3a911e180a09def5c81e902a71d9986b7b51132
abunuwas <joseharoperalta@gmail.com>
2018-04-10 23:10:54 +0100

    Add interfaces for adding logical user

---
+++ src/components/logicalUsers/AddLogicalUser.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <h1>Choose the type of profile</h1>
+    <ul class="nav nav-tabs nav-fill" style="margin-top: 5%;">
+      <li class="nav-item">
+        <router-link :to="{ name: 'createCandidateUser' }" style="text-decoration:none; color:inherit;">
+          <div class="nav-link">
+            <h3>Candidate</h3>
+          </div>
+        </router-link>
+      </li>
+      <li class="nav-item dropdown">
+        <router-link :to="{ name: 'createCandidateUser' }" style="text-decoration:none; color:inherit;">
+          <div class="nav-link">
+            <h3>Candidate</h3>
+          </div>
+        </router-link>
+      </li>
+      <li class="nav-item">
+        <router-link :to="{ name: 'createCandidateUser' }" style="text-decoration:none; color:inherit;">
+          <div class="nav-link">
+            <h3>Candidate</h3>
+          </div>
+        </router-link>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  name: 'add-user-form'
+}
+</script>
+<style>
+  .card-columns {
+  @include media-breakpoint-only(lg) {
+    column-count: 4;
+  }
+  @include media-breakpoint-only(xl) {
+    column-count: 5;
+  }
+  }
+</style>
---
+++ src/views/main/PhysicalUserAddProfile.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <AddLogicalUser />
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+
+import { AddLogicalUser } from '../../components/logicalUsers'
+
+export default {
+  name: 'add-user-profile',
+  components: {
+    AddLogicalUser
+    }
+}
+</script>
