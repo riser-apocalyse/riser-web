99af65291584ffe98f1c62ec7a1a455704c3eb6c
abunuwas <joseharoperalta@gmail.com>
2018-06-23 19:09:57 +0100

    Use namespaced vuex modules and add cognito module

--- src/store.js
+++ src/store.js
@@ -1,12 +1,22 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import { default as vuex } from './vuex'
+// import { default as vuex } from './vuex'
+// import createLogger from 'vuex/dist/logger'
+
+import cognitoConfig from '../config/cognito'
+import CognitoAuth from 'vue-auth-cognito'
+
+import { default as timesheets } from './components/timesheets/vuex'
+// import { default as logicalUsers } from './components/logicalUsers/vuex'
 
 Vue.use(Vuex)
 
 const debug = process.env.NODE_ENV !== 'production'
 
 export default new Vuex.Store({
-  modules: vuex,
+  modules: {
+    timesheets: timesheets,
+    cognito: new CognitoAuth(cognitoConfig)
+  },
   strict: debug
 })
