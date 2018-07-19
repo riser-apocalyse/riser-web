d558d6be4fad4bf3b6b974f7f3be1d498f1e356a
abunuwas <joseharoperalta@gmail.com>
2018-07-19 20:28:22 +0100

    Add elements to candidate dashboard

--- src/_nav.js
+++ src/_nav.js
@@ -4,6 +4,31 @@ export default {
       name: 'Dashboard',
       url: 'candidate/dashboard',
       icon: 'icon-speedometer'
+    },
+    {
+      name: 'Contracts',
+      url: 'candidate/contracts',
+      icon: 'icon-speedometer'
+    },
+    {
+      name: 'Job Search',
+      url: 'candidate/jobs',
+      icon: 'icon-speedometer'
+    },
+    {
+      name: 'Invoices',
+      url: 'candidate/invoices',
+      icon: 'icon-speedometer'
+    },
+    {
+      name: 'Notifications',
+      url: 'candidate/notifications',
+      icon: 'icon-speedometer'
+    },
+    {
+      name: 'Profile Settings',
+      url: 'candidate/profile',
+      icon: 'icon-speedometer'
     }
   ]
 }
