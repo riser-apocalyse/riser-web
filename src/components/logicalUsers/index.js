fb8441ce0f32e563d3b253b70cdaad92e16dfef0
abunuwas <joseharoperalta@gmail.com>
2018-07-19 20:28:09 +0100

    Delete deprecated modules

--- src/components/logicalUsers/index.js
+++
@@ -1,5 +0,0 @@
-import ListLogicalUsers from './ListLogicalUsers.vue'
-import AddLogicalUser from './AddLogicalUser.vue'
-// import DetailsLogicalUsers from './details/DetailsLogicalUsers'
-
-export { ListLogicalUsers, AddLogicalUser } //DetailsLogicalUsers }
--- src/components/logicalUsers/vuex/getters.js
+++
@@ -1,7 +0,0 @@
-export default {
-  getUserById: (state, getters) => timesheetId => {
-    return state.timesheets && timesheetId in state.timesheets
-      ? state.timesheets[timesheetId]
-      : false
-  }
-}
